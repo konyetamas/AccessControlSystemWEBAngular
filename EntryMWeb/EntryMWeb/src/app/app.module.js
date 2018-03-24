fe8651016b36b022de5da1875e002afa81a2fb84
ktamas <konyetomi@gmail.com>
2018-03-24 16:58:54 +0100

    set login page

--- EntryMWeb/EntryMWeb/src/app/app.module.js
+++ EntryMWeb/EntryMWeb/src/app/app.module.js
@@ -11,33 +11,88 @@ var platform_browser_1 = require("@angular/platform-browser");
 var app_component_1 = require("./app.component");
 var Login_component_1 = require("./Login/Login.component");
 var forms_1 = require("@angular/forms");
-//import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-//import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-//import { MatButtonModule, MatCheckboxModule } from '@angular/material';
-var button_1 = require("@angular/material/button");
+var animations_1 = require("@angular/platform-browser/animations");
+var animations_2 = require("@angular/platform-browser/animations");
+//import { MdButtonModule, MdCardModule, MdMenuModule, MdToolbarModule, MdIconModule } from '@angular/material';
+//import { MatButtonModule } from '@angular/material';
 var router_1 = require("@angular/router");
 var http_1 = require("@angular/http");
+//import { MatListModule } from '@angular/material/list';
+//import "~@angular/material/prebuilt-themes/indigo-pink.css";
+var material_1 = require("@angular/material");
 var AppModule = /** @class */ (function () {
+    //@NgModule({
+    //    imports: [
+    //        BrowserModule,
+    //        FormsModule,
+    //        HttpModule,
+    //        //MatListModule,
+    //        //BrowserAnimationsModule,
+    //        //NoopAnimationsModule,
+    //        //MdButtonModule,
+    //        //BrowserAnimationsModule,
+    //        //NoopAnimationsModule,
+    //        MatButtonModule,
+    //      // MatCheckboxModule,
+    //        RouterModule.forRoot([
+    //            { path: "", component: LoginComponent },
+    //        ])
+    //    ],
+    //    declarations: [AppComponent, LoginComponent],
+    //  bootstrap:    [ AppComponent ]
+    //})
     function AppModule() {
     }
     AppModule = __decorate([
         core_1.NgModule({
-            imports: [
-                platform_browser_1.BrowserModule,
+            imports: [platform_browser_1.BrowserModule,
                 forms_1.FormsModule,
                 http_1.HttpModule,
-                button_1.MatButtonModule,
-                //BrowserAnimationsModule,
-                //NoopAnimationsModule,
-                //MatButtonModule,
-                // MatCheckboxModule,
+                animations_1.BrowserAnimationsModule,
+                animations_2.NoopAnimationsModule,
+                material_1.MatButtonModule,
+                material_1.MatMenuModule,
+                material_1.MatToolbarModule,
+                material_1.MatIconModule,
+                material_1.MatCardModule,
+                material_1.MatFormFieldModule,
+                material_1.MatInputModule,
                 router_1.RouterModule.forRoot([
                     { path: "", component: Login_component_1.LoginComponent },
                 ])
             ],
+            exports: [
+                material_1.MatButtonModule,
+                material_1.MatMenuModule,
+                material_1.MatToolbarModule,
+                material_1.MatIconModule,
+                material_1.MatCardModule,
+                material_1.MatInputModule,
+                material_1.MatFormFieldModule,
+            ],
             declarations: [app_component_1.AppComponent, Login_component_1.LoginComponent],
             bootstrap: [app_component_1.AppComponent]
         })
+        //@NgModule({
+        //    imports: [
+        //        BrowserModule,
+        //        FormsModule,
+        //        HttpModule,
+        //        //MatListModule,
+        //        //BrowserAnimationsModule,
+        //        //NoopAnimationsModule,
+        //        //MdButtonModule,
+        //        //BrowserAnimationsModule,
+        //        //NoopAnimationsModule,
+        //        MatButtonModule,
+        //      // MatCheckboxModule,
+        //        RouterModule.forRoot([
+        //            { path: "", component: LoginComponent },
+        //        ])
+        //    ],
+        //    declarations: [AppComponent, LoginComponent],
+        //  bootstrap:    [ AppComponent ]
+        //})
     ], AppModule);
     return AppModule;
 }());
--- EntryMWeb/EntryMWeb/src/app/app.module.ts
+++ EntryMWeb/EntryMWeb/src/app/app.module.ts
@@ -5,30 +5,81 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './Login/Login.component';
 import { FormsModule } from '@angular/forms';
 
-//import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-//import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-//import { MatButtonModule, MatCheckboxModule } from '@angular/material';
-
-import { MatButtonModule } from '@angular/material/button';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+//import { MdButtonModule, MdCardModule, MdMenuModule, MdToolbarModule, MdIconModule } from '@angular/material';
+//import { MatButtonModule } from '@angular/material';
 import { RouterModule } from '@angular/router';
 import { HttpModule } from '@angular/http';
 
+import { CdkTable } from '@angular/cdk/table'
+import { CdkTableModule } from '@angular/cdk/table';
+//import { MatListModule } from '@angular/material/list';
+
+//import "~@angular/material/prebuilt-themes/indigo-pink.css";
+
+
+import {
+    MatButtonModule,
+    MatMenuModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatCardModule,
+    MatInputModule,
+    MatSelectModule,
+    MatFormFieldModule,    
+} from '@angular/material';
+
 @NgModule({
-    imports: [
-        BrowserModule,
+    imports: [BrowserModule,
         FormsModule,
         HttpModule,
+        BrowserAnimationsModule,
+        NoopAnimationsModule,
         MatButtonModule,
-        //BrowserAnimationsModule,
-        //NoopAnimationsModule,
-        //MatButtonModule,
-       // MatCheckboxModule,
+        MatMenuModule,
+        MatToolbarModule,
+        MatIconModule,
+        MatCardModule,
+        MatFormFieldModule,
+        MatInputModule,
         RouterModule.forRoot([
             { path: "", component: LoginComponent },
-            
+
         ])
     ],
+    exports: [
+        MatButtonModule,
+        MatMenuModule,
+        MatToolbarModule,
+        MatIconModule,
+        MatCardModule,
+        MatInputModule,
+        MatFormFieldModule,
+    ],
     declarations: [AppComponent, LoginComponent],
-  bootstrap:    [ AppComponent ]
+    bootstrap: [AppComponent]
 })
+
+//@NgModule({
+//    imports: [
+//        BrowserModule,
+//        FormsModule,
+//        HttpModule,
+//        //MatListModule,
+//        //BrowserAnimationsModule,
+//        //NoopAnimationsModule,
+//        //MdButtonModule,
+//        //BrowserAnimationsModule,
+//        //NoopAnimationsModule,
+//        MatButtonModule,
+//      // MatCheckboxModule,
+//        RouterModule.forRoot([
+//            { path: "", component: LoginComponent },
+            
+//        ])
+//    ],
+//    declarations: [AppComponent, LoginComponent],
+//  bootstrap:    [ AppComponent ]
+//})
 export class AppModule { }
